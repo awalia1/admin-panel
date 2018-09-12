c09949112ee36f168184620df04726f3bfa0e6ea
Amarpreet Walia <amarpreet.s.walia@gmail.com>
2018-09-11 23:54:15 -0400

    added user table

---
+++ app/models/user.rb
@@ -0,0 +1,27 @@
+class User < ApplicationRecord
+    before_save { self.email = email.downcase }
+    has_one_attached :photo
+    has_many :articles
+    has_secure_password
+    validates :password, presence: true, length: { maximum: 32, minimum: 4 }
+    validates :email, presence: true, length: { maximum: 100 }
+    def full_name
+    self.first_name + " " + self.last_name
+    end
+
+    def generate_eid
+    self.eid = SecureRandom.hex(3).upcase
+    end
+
+    def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+    end
+
+    def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
+  get 'users/edit'
+  get 'users/show'
+  get 'users/index'
   root 'pages#home'
+
+  get 'register' => 'users#new'
+
+  get '/login' => 'sessions#new'
   get 'pages/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
